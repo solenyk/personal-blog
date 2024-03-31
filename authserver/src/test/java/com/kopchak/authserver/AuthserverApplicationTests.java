a4a3e49f120d477c957297f3603257b4c54b3c1f
Iryna Kopchak <iryna.kopchak39@gmail.com>
2024-03-31 17:29:53 +0300

    build: add authserver module

---
+++ authserver/src/test/java/com/kopchak/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopchak.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
