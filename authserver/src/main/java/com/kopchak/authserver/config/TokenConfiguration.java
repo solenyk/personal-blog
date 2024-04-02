53c167115e040394b2d31eb9e1b6bef043faca07
Iryna Kopchak <iryna.kopchak39@gmail.com>
2024-04-02 19:52:12 +0300

    feat: add tokenCustomizer bean to the TokenConfiguration

--- authserver/src/main/java/com/kopchak/authserver/config/TokenConfiguration.java
+++ authserver/src/main/java/com/kopchak/authserver/config/TokenConfiguration.java
@@ -1,5 +1,6 @@
 package com.kopchak.authserver.config;
 
+import com.nimbusds.jose.Algorithm;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
@@ -7,14 +8,21 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Configuration
 public class TokenConfiguration {
@@ -46,5 +54,18 @@ public class TokenConfiguration {
     @Bean
     public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
         return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(UserDetailsService userDetailsService) {
+        return (context) -> {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(context.getPrincipal().getName());
+            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+            context.getClaims().claims(claims ->
+                    claims.put("authorities", authorities.stream()
+                            .map(GrantedAuthority::getAuthority)
+                            .collect(Collectors.toList()))
+            );
+        };
     }
 }
