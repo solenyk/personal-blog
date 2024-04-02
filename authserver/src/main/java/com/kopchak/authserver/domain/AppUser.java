c5ff11419a1905c6d429e507ec9f4225a066605d
Iryna Kopchak <iryna.kopchak39@gmail.com>
2024-04-02 18:05:51 +0300

    feat: add Role enum and AppUser entity

---
+++ authserver/src/main/java/com/kopchak/authserver/domain/AppUser.java
@@ -0,0 +1,58 @@
+package com.kopchak.authserver.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
+public class AppUser implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 120, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 60, nullable = false)
+    private String password;
+
+    @Column(length = 10, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ authserver/src/main/java/com/kopchak/authserver/domain/Role.java
@@ -0,0 +1,6 @@
+package com.kopchak.authserver.domain;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
