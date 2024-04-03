1eb87e8414043206c9f4a8a65fe3da60ffe4ed52
Iryna Kopchak <iryna.kopchak39@gmail.com>
2024-04-03 16:38:34 +0300

    feat: add CustomOAuth2UserService and CustomOidcUserService

---
+++ resourceserver/src/main/java/com/kopchak/resourceserver/service/CustomOidcUserService.java
@@ -0,0 +1,37 @@
+package com.kopchak.resourceserver.service;
+
+import com.kopchak.resourceserver.domain.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+public class CustomOidcUserService extends OidcUserService {
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        String clientName = userRequest.getClientRegistration().getClientName();
+        OidcUser user = super.loadUser(userRequest);
+
+        Map<String, Object> claims = userRequest.getIdToken().getClaims();
+        OidcUserInfo userInfo = new OidcUserInfo(claims);
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        authorities.add(new OidcUserAuthority(userRequest.getIdToken(), userInfo));
+        OAuth2AccessToken token = userRequest.getAccessToken();
+        for (String scope : token.getScopes()) {
+            authorities.add(new SimpleGrantedAuthority("SCOPE_" + scope));
+        }
+        return new AppUser(clientName, user.getName(), authorities);
+    }
+}
