0c8d0e904bc49cc1721919c14effb45290f0491c
Iryna Kopchak <iryna.kopchak39@gmail.com>
2024-04-03 16:42:07 +0300

    feat: add ResourceServerConfig

---
+++ resourceserver/src/main/java/com/kopchak/resourceserver/config/ResourceServerConfig.java
@@ -0,0 +1,53 @@
+package com.kopchak.resourceserver.config;
+
+import com.kopchak.resourceserver.service.CustomOAuth2UserService;
+import com.kopchak.resourceserver.service.CustomOidcUserService;
+import com.kopchak.resourceserver.service.JwtToOAuth2Converter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ResourceServerConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOidcUserService customOidcUserService;
+    private final JwtToOAuth2Converter jwtToOAuth2Converter;
+
+    @Value("${JWK_SET_URI}")
+    private String jwkSetUri;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.decoder(jwtDecoder())
+                        .jwtAuthenticationConverter(jwtToOAuth2Converter)
+                )
+        ).oauth2Login(oauth -> oauth
+                .userInfoEndpoint(userInfo -> userInfo
+                        .userService(customOAuth2UserService)
+                        .oidcUserService(customOidcUserService)
+                )
+        ).authorizeHttpRequests(matcherRegistry -> matcherRegistry
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withJwkSetUri(jwkSetUri)
+                .jwsAlgorithm(SignatureAlgorithm.RS256).build();
+    }
+}
