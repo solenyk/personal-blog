a8d589c88f4fe8572e765e4e9cd076093fbc9018
Iryna Kopchak <iryna.kopchak39@gmail.com>
2024-04-11 20:40:31 +0300

    feat: delete UserServiceIntegrationTest and ServiceRepoIntegrationTestSuite

--- authserver/src/test/java/com/kopchak/authserver/integration/ServiceRepoIntegrationTestSuite.java
+++
@@ -1,9 +0,0 @@
-package com.kopchak.authserver.integration;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectPackages({"com.kopchak.authserver.integration.repository", "com.kopchak.authserver.integration.service"})
-public class ServiceRepoIntegrationTestSuite {
-}
